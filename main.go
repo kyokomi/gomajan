25d308d7a18912f34efd5675ca47e28fc73a133b
kyokomi <kyoko1220adword@gmail.com>
2014-11-04 08:24:59 +0900

    refactor: player

--- main.go
+++ main.go
@@ -5,45 +5,6 @@ import (
 	"github.com/kyokomi/gomajan/mjp"
 )
 
-type Tehai struct {
-	pai mjp.MJP
-	val int
-}
-
-type Player struct {
-	// 手牌
-	tiles []Tehai
-	// フーロ
-	foo [][]mjp.MJP
-}
-
-func NewPlayer() Player {
-	p := Player{}
-	// 33種類
-	p.tiles = NewTehai(nil)
-	// 最大4フーロ
-	p.foo = make([][]mjp.MJP, 4)
-
-	// TODO: ランダムな牌を設定する
-
-	return p
-}
-
-func NewTehai(tehai map[mjp.MJP]int) []Tehai {
-	tiles := make([]Tehai, 34)
-	for i := 0; i < 34; i++ {
-		tiles[i].pai = mjp.MJP(i)
-
-		if tehai != nil && tehai[tiles[i].pai] > 0 {
-			tiles[i].val = tehai[tiles[i].pai]
-		} else {
-			tiles[i].val = 0
-		}
-	}
-
-	return tiles
-}
-
 // 国士無双.
 func (p Player) isKokushimusou() bool {
 	if p.tiles[mjp.M1].val >= 1 &&
@@ -91,20 +52,6 @@ func (p Player) isNikoNiko() bool {
 		count++
 	}
 	return count == 7
-}
-
-func (p Player) String() string {
-	var tehaiStr string
-	// 手牌を表示
-	for _, tehai := range p.tiles {
-		if tehai.val <= 0 {
-			continue
-		}
-		for i := 0; i < tehai.val; i++ {
-			tehaiStr += tehai.pai.String() + " "
-		}
-	}
-	return tehaiStr
 }
 
 func main() {
