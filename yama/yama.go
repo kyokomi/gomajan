e668c2687790e430f3ba7b471b6c0553c5583c19
kyokomi <kyoko1220adword@gmail.com>
2015-03-15 19:14:18 +0900

    山生成を実装

---
+++ yama/yama.go
@@ -0,0 +1,61 @@
+package yama
+
+import (
+	"math/rand"
+
+	"github.com/kyokomi/gomajan/mjp/pai"
+	"github.com/kyokomi/gomajan/mjp/tehai"
+	"fmt"
+	"time"
+)
+
+type Yama [4][2][17]pai.MJP
+
+var takuName = map[int]string{
+	1: "東",
+	2: "南",
+	3: "西",
+	4: "北",
+}
+
+func New() Yama {
+	pais := tehai.NewTehai(nil)
+
+	rand.Seed(time.Now().UnixNano())
+
+	var y Yama
+	for i := 0; i < len(y); i++ {
+		for j := 0; j < len(y[i]); j++ {
+			for k := 0; k < len(y[i][j]); k++ {
+			RAND:
+				p := pai.MJP(rand.Intn(pai.PaiSize() -1)) + 1
+				if pais[p].Val == 4 {
+					goto RAND
+				}
+				y[i][j][k] = p
+				pais[p].Val++
+			}
+		}
+	}
+
+	// debug log
+
+	for idx, yy := range y {
+		for i, yyy := range yy {
+			if i == 0 {
+				fmt.Print(takuName[idx+1] + " ")
+			} else {
+				fmt.Print("   ")
+			}
+
+			for _, yyyy := range yyy {
+				fmt.Print(yyyy)
+				fmt.Print(" ")
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+	}
+
+	return y
+}
---
+++ yama/yama_test.go
@@ -0,0 +1,33 @@
+package yama
+import (
+	"testing"
+	"github.com/kyokomi/gomajan/mjp/tehai"
+	"github.com/kyokomi/gomajan/mjp/pai"
+)
+
+func TestYama(t *testing.T) {
+	// test
+
+	y := New()
+
+	pais := tehai.NewTakuPai()
+	for i := 0; i < len(y); i++ {
+		for j := 0; j < len(y[i]); j++ {
+			for k := 0; k < len(y[i][j]); k++ {
+				pais[y[i][j][k]].Val--
+			}
+		}
+	}
+
+	// check
+
+	for _, p := range pais {
+		if p.Pai == pai.NonePai {
+			continue
+		}
+
+		if p.Val != 0 {
+			t.Errorf("4枚じゃないです %s = %d", p.Pai, p.Val)
+		}
+	}
+}
