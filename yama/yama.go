3a8673688388e95cbe7f4aa7eb926b42c0d6d09c
kyokomi <kyoko1220adword@gmail.com>
2015-03-16 22:35:54 +0900

    山から配牌つくってみた(おためし

--- yama/yama.go
+++ yama/yama.go
@@ -42,13 +42,15 @@ func New() Yama {
 	}
 
 	// debug log
+	fmt.Println("     00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16")
+	fmt.Println("-------------------------------------------------------")
 
 	for idx, yy := range y {
 		for i, yyy := range yy {
 			if i == 0 {
-				fmt.Print(takuName[idx+1] + " ")
+				fmt.Print(idx, " " + takuName[idx+1] + " ")
 			} else {
-				fmt.Print("   ")
+				fmt.Print("     ")
 			}
 
 			for _, yyyy := range yyy {
