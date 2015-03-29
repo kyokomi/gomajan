634ce7cfe4229c11c559acc31b186f24158ff7be
kyokomi <kyoko1220adword@gmail.com>
2015-03-29 21:06:01 +0900

    適当にアガリまで目指すのを試しに書いてみた

--- taku/taku_test.go
+++ taku/taku_test.go
@@ -19,4 +19,43 @@ func TestDoCalcPoint(t *testing.T) {
 	for _, p := range taku.Players {
 		fmt.Printf("player%d 配牌: %s\n", p.PlayerID(), p.String())
 	}
+
+	ueshita := 2
+	nextFunc := func(playerID int) pai.MJP {
+		if ueshita == 2 {
+			taku.Add列And山越しFunc(1)
+			ueshita = 0
+		}
+
+		p := taku.Next(ueshita, playerID)
+		ueshita++
+		return p
+	}
+
+	for {
+		p := nextFunc(1)
+		if p == pai.NonePai {
+			break
+		}
+
+		fmt.Println("引いた牌: ", p)
+
+		taku.Players[1].PaiInc(p)
+		yakuCheck := taku.Players[1].NewYakuCheck(p)
+		if yakuCheck.Is和了() {
+			fmt.Println("アガリ: ", yakuCheck.String())
+			break
+		}
+		fmt.Println("チェック: ", yakuCheck.String())
+
+		for _, noko := range yakuCheck.MentsuCheck().Nokori() {
+			if noko.Pai.Type() == pai.NoneType || noko.Val == 0 {
+				continue
+			}
+
+			fmt.Println("捨てた牌: ", noko.Pai)
+			taku.Players[1].PaiDec(noko.Pai)
+			break
+		}
+	}
 }
